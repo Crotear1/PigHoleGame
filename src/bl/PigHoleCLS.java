fde8c2e8f53a96b0fe91ec24edadf62435144362
KrizanicS <sascha.krizanic@xal.com>
2023-11-09 09:07:48 +0100

    add logic functions bl

--- src/bl/PigHoleCLS.java
+++ src/bl/PigHoleCLS.java
@@ -1,4 +1,25 @@
 package src.bl;
 
+import java.util.Random;
+
 public class PigHoleCLS {
+    private int[] fieldArray = new int[6];
+
+    public int rollDice() {
+        return new Random().nextInt(6) + 1;
+    }
+
+    public boolean rollDiceCheck() {
+        int fullDices = 0;
+        for (int i = 0; i < 6; i++) {
+            if (fieldArray[i] == i + 1) {
+                fullDices++;
+            }
+        }
+        double throwDiceChance = (fullDices / 6) * 100;
+        if (throwDiceChance > 50) {
+            return true;
+        }
+        return false;
+    }
 }
