91b206c2ad2584cee2a60980db270d8c4e1283cf
Jakob Lipp <jakoblipp03@gmail.com>
2023-11-09 08:17:07 +0100

    Insert Main method to PigHoleGUI.java

--- src/ui/PigHoleGUI.java
+++ src/ui/PigHoleGUI.java
@@ -1,4 +1,17 @@
 package src.ui;
 
+import javax.swing.*;
+
 public class PigHoleGUI {
+
+
+    private JPanel mainPanel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Pig Hole");
+        frame.setContentPane(new PigHoleGUI().mainPanel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
 }
